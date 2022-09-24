8e13cfb1b8d41f80d7ad8dc6f6ef91bba3780874
tharusha-rasanjana <114277178+tharusha-rasanjana@users.noreply.github.com>
2022-09-24 11:51:59 +0530

    Add files via upload

---
+++ Run-Game.bat
@@ -0,0 +1,27 @@
+@echo off
+echo.  =====================
+echo.  = SUPER MINDER V1.0 =
+echo.  =====================
+
+echo.  ======================
+echo.  = WINDOWS BAT RUNNER =
+echo.  ======================
+
+echo. =====================================
+echo. = CREATED BY W.H.Tharusha Rasanjana =
+echo. =====================================
+echo. All Rights Reserved!
+
+echo [=] Instructions Before Play The Game
+echo [-] Password Of The Game Is 4455
+echo [-] This Game Needs A Nickname To Play
+echo [-] Therefore Add A Nickname To Game To Play 
+echo [-] And Also This Game Is Recommended For 10+ 
+echo [-] Therefore The Age Is Highly Required
+echo [-] Therefore Add The Year Which You Born
+echo [-] Enter Correct Answer To Procceed To Next Mission
+echo [-] If Your Answers Or Any Terms Of Conditions Are Wrong The Game Will Exit Automatically
+echo [-] Thank You And Good Job
+ 
+echo Starting Game...
+"Super Minder".exe
